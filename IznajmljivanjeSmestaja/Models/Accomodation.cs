d38fe757652d33ae6ce1565889ee8a0193375f34
Nadica Zarkic <nadicazarkic@gmail.com>
2021-02-02 16:14:44 +0100

    dodavanje baze

---
+++ IznajmljivanjeSmestaja/Models/Accomodation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IznajmljivanjeSmestaja.Models
+{
+    public partial class Accomodation
+    {
+        public int Id { get; set; }
+        public int Rooms { get; set; }
+        public int Guests { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Amenities { get; set; }
+        public string Directions { get; set; }
+        public string Checkin { get; set; }
+        public string Checkout { get; set; }
+        public bool? Wifi { get; set; }
+        public int? IdUser { get; set; }
+    }
+}
---
+++ IznajmljivanjeSmestaja/Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IznajmljivanjeSmestaja.Models
+{
+    public partial class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime DateCheckin { get; set; }
+        public DateTime DateCheckout { get; set; }
+        public int IdAccomodation { get; set; }
+        public int IdUser { get; set; }
+    }
+}
